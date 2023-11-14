210d1b652818c05ce8d4ccfa8706c017eff833ec
ShishirShekhar <sspdav02@gmail.com>
2023-11-14 16:22:11 +0530

    created basic hero section

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Nav from './Nav';
+import Nav from "./Nav";
 
 const Hero = () => {
   return (
-    <div className='h-screen relative'>
+    <div className="hero h-screen text-white">
       <Nav />
-      Hero
+      <div className="h-full w-full flex flex-col items-center justify-center ">
+        <p className="text-7xl pb-10 w-1/2 text-center">
+          Reliable and Durable Machine Tools
+        </p>
+        <button className="bg-blue-500 px-4 py-2 rounded-xl">
+          Enquire Now
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
--- client/src/components/Home/Nav.jsx
+++ client/src/components/Home/Nav.jsx
@@ -2,7 +2,7 @@ import logo from "../../images/logo.png";
 
 const Nav = () => {
   return (
-    <div className="bg-black bg-opacity-50 px-10 py-4 text-white flex justify-between items-center w-full fixed">
+    <div className="bg-black bg-opacity-50 px-10 py-4 text-white flex justify-between items-center w-full fixed z-10">
       <img src={logo} alt="logo" className="w-10" />
       <div className="flex justify-evenly w-2/3">
         <p>
