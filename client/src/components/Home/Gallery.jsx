44c13b0127a484431e18cdaa58406c357eabb466
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 16:28:20 +0530

    alinged text center

--- client/src/components/Home/Gallery.jsx
+++ client/src/components/Home/Gallery.jsx
@@ -4,7 +4,7 @@ import { products } from "../../constants/products";
 const Gallery = () => {
   return (
     <div className="py-10 px-10" id="gallery">
-      <h1 className="text-4xl">Gallery</h1>
+      <h1 className="text-4xl text-center font-semibold">Gallery</h1>
       <div className="flex flex-wrap justify-evenly">
         {products.map((product) => {
           return (
