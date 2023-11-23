0e4753f91d15506d2f8a06c2b308d268c801901a
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 14:07:02 +0530

    update product details

--- client/src/constants/products.js
+++ client/src/constants/products.js
@@ -19,38 +19,38 @@ export const popularProducts = [
 
 export const bestProducts = [
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "V Belt Lathe Machine",
+    img: "https://4.imimg.com/data4/CL/HS/MY-6340428/heavy-duty-v-belt-drive-lathe-machine.jpg",
   },
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "V Belt Lathe Machine",
+    img: "https://4.imimg.com/data4/CL/HS/MY-6340428/heavy-duty-v-belt-drive-lathe-machine.jpg",
   },
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "V Belt Lathe Machine",
+    img: "https://4.imimg.com/data4/CL/HS/MY-6340428/heavy-duty-v-belt-drive-lathe-machine.jpg",
   },
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "V Belt Lathe Machine",
+    img: "https://4.imimg.com/data4/CL/HS/MY-6340428/heavy-duty-v-belt-drive-lathe-machine.jpg",
   },
 ];
 
 export const featuredProducts = [
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "Hydraulic Press Machine",
+    img: "https://cpimg.tistatic.com/05707195/b/4/Electric-Hydraulic-Press-Machine.jpg",
   },
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "Hydraulic Press Machine",
+    img: "https://cpimg.tistatic.com/05707195/b/4/Electric-Hydraulic-Press-Machine.jpg",
   },
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "Hydraulic Press Machine",
+    img: "https://cpimg.tistatic.com/05707195/b/4/Electric-Hydraulic-Press-Machine.jpg",
   },
   {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+    name: "Hydraulic Press Machine",
+    img: "https://cpimg.tistatic.com/05707195/b/4/Electric-Hydraulic-Press-Machine.jpg",
   },
 ];
