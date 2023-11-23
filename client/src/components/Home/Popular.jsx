7fe52753084a3863b17ee39f20079bf61c39fabb
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 11:27:11 +0530

    improved hover style

--- client/src/components/Home/Popular.jsx
+++ client/src/components/Home/Popular.jsx
@@ -16,18 +16,14 @@ const popular = [
   {
     name: "Extra Heavy Duty Roll Turning Lathe Machine",
     img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
-  },
-  {
-    name: "Extra Heavy Duty Roll Turning Lathe Machine",
-    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
-  },
+  }
 ];
 
 const Popular = () => {
   return (
-    <div className="py-4 text-4xl">
+    <div className="py-10 text-4xl">
       <h1 className="text-center">Popular Products</h1>
-      <div className="flex justify-around">
+      <div className="flex justify-around py-10">
         {popular.map((product, index) => {
           return (
             <div className="featured-product" key={index + product.name}>
