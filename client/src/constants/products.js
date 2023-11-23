06ac7b40a55979e5c532f61357114f56bc872bd8
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 16:25:44 +0530

    added product array

--- client/src/constants/products.js
+++ client/src/constants/products.js
@@ -54,3 +54,30 @@ export const featuredProducts = [
     img: "https://5.imimg.com/data5/MJ/WO/OW/SELLER-1823380/work-roll-grinder-machines-500x500.jpg",
   },
 ];
+
+export const products = [
+  {
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    img: "https://www.tradeindia.com/_next/image/?url=https%3A%2F%2Fcpimg.tistatic.com%2F05629471%2Fb%2F4%2FHeavy-Duty-Lathe-Machine.jpg&w=750&q=75",
+  },
+  {
+    img: "https://static.machinetools.com/uploads/1971959/FL1600_CNC_Facing_Lathe.jpg",
+  },
+  {
+    img: "https://www.indiantradebird.com/itb-admin/members/1568/images/0_2a94c87b.jpg",
+  },
+  {
+    img: "https://4.imimg.com/data4/CL/HS/MY-6340428/heavy-duty-v-belt-drive-lathe-machine.jpg",
+  },
+  {
+    img: "https://5.imimg.com/data5/IN/XR/DT/SELLER-2361754/shearing-bending-machine.jpeg",
+  },
+  {
+    img: "https://5.imimg.com/data5/SELLER/Default/2020/11/DJ/ZD/YS/1206237/surface-grinder-machine-size-12-x-24.jpg",
+  },
+  {
+    img: "https://ravimachines.com/wp-content/uploads/2016/07/full_Banka.18_shaping.jpg",
+  },
+];
