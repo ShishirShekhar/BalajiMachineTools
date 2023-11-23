5974cdd54c2d7a2c68fbdae37c4dcd15cc1bca81
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 14:23:06 +0530

    updated background for 2n+1

--- client/src/components/Home/FeaturedProduct.jsx
+++ client/src/components/Home/FeaturedProduct.jsx
@@ -1,6 +1,6 @@
 const FeaturedProduct = (props) => {
   return (
-    <div className="py-10 text-4xl">
+    <div className="py-10 text-4xl featured-products">
       <h1 className="text-center">{props.title}</h1>
       <div className="flex justify-around py-10">
         {props.products.map((product, index) => {
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,32 @@
-import Hero from "../components/Home/Hero"
+import Hero from "../components/Home/Hero";
 import About from "../components/Home/About";
 import Gallery from "../components/Home/Gallery";
 import Contact from "../components/Home/Contact";
 import FeaturedProduct from "../components/Home/FeaturedProduct";
-import { popularProducts, bestProducts, featuredProducts } from "../constants/products";
+import {
+  popularProducts,
+  bestProducts,
+  featuredProducts,
+} from "../constants/products";
 
 const Home = () => {
   return (
     <div id="home">
       <Hero />
-      <FeaturedProduct title={"Popular Products"} products={popularProducts} />
-      <FeaturedProduct title={"Best Products"} products={bestProducts} />
-      <FeaturedProduct title={"Featured Products"} products={featuredProducts} />
+        <FeaturedProduct
+          title={"Popular Products"}
+          products={popularProducts}
+        />
+        <FeaturedProduct title={"Best Products"} products={bestProducts} />
+        <FeaturedProduct
+          title={"Featured Products"}
+          products={featuredProducts}
+        />
       <About />
       <Gallery />
       <Contact />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
