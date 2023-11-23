c4199c229ed1f020cb17748c5bc7e4bae8c9f339
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 15:41:06 +0530

    added padding and changed font weight

--- client/src/components/Home/FeaturedProduct.jsx
+++ client/src/components/Home/FeaturedProduct.jsx
@@ -1,7 +1,7 @@
 const FeaturedProduct = (props) => {
   return (
-    <div className="py-10 text-4xl featured-products">
-      <h1 className="text-center">{props.title}</h1>
+    <div className="py-10 px-10 text-4xl featured-products">
+      <h1 className="text-center font-semibold">{props.title}</h1>
       <div className="flex justify-around py-10">
         {props.products.map((product, index) => {
           return (
