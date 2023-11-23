94e4ce8c377dedd3aff9f6b5cdd19698a91554d3
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 11:19:36 +0530

    created basic popular product section

--- client/src/components/Home/Popular.jsx
+++ client/src/components/Home/Popular.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+
+const popular = [
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+];
 
 const Popular = () => {
   return (
-    <div>Popular</div>
-  )
-}
+    <div className="py-4 text-4xl">
+      <h1 className="text-center">Popular Products</h1>
+      <div className="flex justify-around">
+        {popular.map((product, index) => {
+          return (
+            <div className="featured-product" key={index + product.name}>
+              <img src={product.img} alt={product.name} />
+              <div className="overlay">
+                <p className="text">{product.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Popular
+export default Popular;
