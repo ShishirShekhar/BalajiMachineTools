b7dc6d02f791cd26c5c391ff34ba3bc635a623f7
ShishirShekhar <sspdav02@gmail.com>
2023-11-23 11:34:24 +0530

    add folder for constants

---
+++ client/src/constants/products.js
@@ -0,0 +1,56 @@
+export const popularProducts = [
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+];
+
+export const bestProducts = [
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+];
+
+export const featuredProducts = [
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+  {
+    name: "Extra Heavy Duty Roll Turning Lathe Machine",
+    img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
+  },
+];
