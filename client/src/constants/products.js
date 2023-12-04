c12e193796d20598589c40d6239fdb40b4c77f7a
ShishirShekhar <sspdav02@gmail.com>
2023-12-04 12:08:12 +0530

    add product id for each product

--- client/src/constants/products.js
+++ client/src/constants/products.js
@@ -1,17 +1,21 @@
 export const popularProducts = [
   {
+    id: "123",
     name: "Extra Heavy Duty Roll Turning Lathe Machine",
     img: "https://static.connect2india.com/c2icd/company_resources/1035057/images/products/product-extra-heavy-duty-roll-turning-lathe-machine.jpg",
   },
   {
+    id: "123",
     name: "Heavy Duty Lathe Machine",
     img: "https://www.tradeindia.com/_next/image/?url=https%3A%2F%2Fcpimg.tistatic.com%2F05629471%2Fb%2F4%2FHeavy-Duty-Lathe-Machine.jpg&w=750&q=75",
   },
   {
+    id: "123",
     name: "Facing Lathe Machine",
     img: "https://static.machinetools.com/uploads/1971959/FL1600_CNC_Facing_Lathe.jpg",
   },
   {
+    id: "123",
     name: "Roll Turning Lathe Machine",
     img: "https://www.indiantradebird.com/itb-admin/members/1568/images/0_2a94c87b.jpg",
   },
@@ -19,18 +23,22 @@ export const popularProducts = [
 
 export const bestProducts = [
   {
+    id: "123",
     name: "V Belt Lathe Machine",
     img: "https://4.imimg.com/data4/CL/HS/MY-6340428/heavy-duty-v-belt-drive-lathe-machine.jpg",
   },
   {
+    id: "123",
     name: "Sheet Bending and Shearing Machine",
     img: "https://5.imimg.com/data5/IN/XR/DT/SELLER-2361754/shearing-bending-machine.jpeg",
   },
   {
+    id: "123",
     name: "Plano Milling Machine",
     img: "https://cpimg.tistatic.com/06642852/b/4/Plano-Milling-Machine.jpg",
   },
   {
+    id: "123",
     name: "Shaping Machine",
     img: "https://ravimachines.com/wp-content/uploads/2016/07/full_Banka.18_shaping.jpg",
   },
@@ -38,18 +46,22 @@ export const bestProducts = [
 
 export const featuredProducts = [
   {
+    id: "123",
     name: "Hydraulic Press Machine",
     img: "https://cpimg.tistatic.com/05707195/b/4/Electric-Hydraulic-Press-Machine.jpg",
   },
   {
+    id: "123",
     name: "Industrial Milling Machine",
     img: "https://4.imimg.com/data4/DM/QU/MY-1129710/industrial-milling-machine.jpg",
   },
   {
+    id: "123",
     name: "Surface Grinder Machine",
     img: "https://5.imimg.com/data5/SELLER/Default/2020/11/DJ/ZD/YS/1206237/surface-grinder-machine-size-12-x-24.jpg",
   },
   {
+    id: "123",
     name: "Industrial Roll Grinding Machine",
     img: "https://5.imimg.com/data5/MJ/WO/OW/SELLER-1823380/work-roll-grinder-machines-500x500.jpg",
   },
