9bb003ff2b6c74e70be4d829a877fe091a47dfbf
ShishirShekhar <sspdav02@gmail.com>
2023-12-04 12:16:11 +0530

    add product id for each product

--- client/src/constants/products.js
+++ client/src/constants/products.js
@@ -80,24 +80,31 @@ export const products = [
     maxPrice: 5000000,
   },
   {
+    id: "123",
     img: "https://www.tradeindia.com/_next/image/?url=https%3A%2F%2Fcpimg.tistatic.com%2F05629471%2Fb%2F4%2FHeavy-Duty-Lathe-Machine.jpg&w=750&q=75",
   },
   {
+    id: "123",
     img: "https://static.machinetools.com/uploads/1971959/FL1600_CNC_Facing_Lathe.jpg",
   },
   {
+    id: "123",
     img: "https://www.indiantradebird.com/itb-admin/members/1568/images/0_2a94c87b.jpg",
   },
   {
+    id: "123",
     img: "https://4.imimg.com/data4/CL/HS/MY-6340428/heavy-duty-v-belt-drive-lathe-machine.jpg",
   },
   {
+    id: "123",
     img: "https://5.imimg.com/data5/IN/XR/DT/SELLER-2361754/shearing-bending-machine.jpeg",
   },
   {
+    id: "123",
     img: "https://5.imimg.com/data5/SELLER/Default/2020/11/DJ/ZD/YS/1206237/surface-grinder-machine-size-12-x-24.jpg",
   },
   {
+    id: "123",
     img: "https://ravimachines.com/wp-content/uploads/2016/07/full_Banka.18_shaping.jpg",
   },
 ];
