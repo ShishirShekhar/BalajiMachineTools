dcaf547cd631f6bfa978133b8740cd3cd97f40b4
ShishirShekhar <sspdav02@gmail.com>
2023-12-13 11:41:26 +0530

    add flex wrap for all device size

--- client/src/components/Home/FeaturedProduct.jsx
+++ client/src/components/Home/FeaturedProduct.jsx
@@ -4,7 +4,7 @@ const FeaturedProduct = (props) => {
   return (
     <div className="py-10 px-10 text-4xl featured-products">
       <h1 className="text-center font-semibold">{props.title}</h1>
-      <div className="flex justify-around py-10">
+      <div className="flex flex-wrap gap-5 justify-around py-10">
         {props.products.map((product, index) => {
           return (
             <Link to={`/product/${product.id}`}>
