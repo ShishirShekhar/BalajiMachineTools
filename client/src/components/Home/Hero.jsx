b0490250414a2d3f047cdd577415472b318c7c36
ShishirShekhar <sspdav02@gmail.com>
2023-12-13 12:03:22 +0530

    change hero font size for small device

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="hero h-screen text-white">
       <Nav />
       <div className="h-full w-full flex flex-col items-center justify-center ">
-        <p className="text-7xl pb-10 w-1/2 text-center">
+        <p className="text-6xl sm:text-7xl pb-10 px-4 sm:w-1/2 text-center">
           Reliable and Durable Machine Tools
         </p>
         <button className="bg-blue-500 px-4 py-2 rounded-xl">
