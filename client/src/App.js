6b7c997041d35f11cb46bda729077be8e0c25377
ShishirShekhar <sspdav02@gmail.com>
2023-12-15 00:48:19 +0530

    added not found page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDesc from "./pages/ProductDesc";
 import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 
@@ -11,9 +12,10 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDesc />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/products/:id" element={<ProductDesc />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="h-screen flex justify-center items-center bg-blue-200">
+      <h1 className="text-blue-500 text-5xl font-bold md:text-7xl">Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
