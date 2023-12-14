ce22a019a8a907506977d1478416f9dbf8d1d501
ShishirShekhar <sspdav02@gmail.com>
2023-12-14 23:54:33 +0530

    moving to gallery onclick enquiry

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -5,9 +5,11 @@ const Hero = () => {
         <p className="text-6xl sm:text-7xl pb-10 px-4 sm:w-1/2 text-center">
           Reliable and Durable Machine Tools
         </p>
-        <button className="bg-blue-500 px-4 py-2 rounded-xl">
-          Enquire Now
-        </button>
+        <a href="#gallery">
+          <button className="bg-blue-500 px-4 py-2 rounded-xl">
+            Enquire Now
+          </button>
+        </a>
       </div>
     </div>
   );
