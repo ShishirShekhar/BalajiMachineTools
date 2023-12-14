0cf92a04afc42949ba87afec9e28ef5f812a9a02
ShishirShekhar <sspdav02@gmail.com>
2023-12-15 00:16:05 +0530

    added product not found

--- client/src/pages/ProductDesc.jsx
+++ client/src/pages/ProductDesc.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { Link, useParams } from "react-router-dom";
 import { RiStarSLine, RiStarSFill } from "react-icons/ri";
 import { products } from "../constants/products";
@@ -5,97 +6,115 @@ import { products } from "../constants/products";
 const Product = () => {
   const { id } = useParams();
   const product = products.find((product) => product.id === id);
-  let message = `Hello, I visited this website https://smtlathemachine.vercel.app/ , and I would like to know more about ${product.name}`;
+  let message = `Hello, I visited this website https://smtlathemachine.vercel.app/ , and I would like to know more about ${
+    product?.name || ""
+  }`;
   message = message.replace(/ /g, "%20");
 
   return (
-    <div className="min-h-screen py-10 px-10 ">
-      <div className="flex flex-col md:flex-row">
-        <div className="rounded-xl md:w-1/2 shadow-xl">
-          <img
-            src={product.img}
-            alt={product.name}
-            className="rounded-xl p-10 w-full h-full "
-            loading="lazy"
-          />
+    <div className="py-10 px-10">
+      {product === undefined ? (
+        <div className="flex items-center justify-center h-[30rem] md:h-[32rem]">
+          <h1 className="text-5xl md:text-7xl font-bold text-center">
+            Product Not Found
+          </h1>
         </div>
-
-        <div className="flex flex-col gap-5 py-5 md:py-0 md:px-10 md:w-1/2 justify-evenly">
-          <div>
-            <Link to={"/"}>
-              <p className=" text-blue-500">More products</p>
-            </Link>
-            <h1 className="text-4xl">{product.name}</h1>
-            <div className="flex flex-wrap py-2 items-center">
-              <div className="flex text-yellow-500 items-center pr-4">
-                <span className="pr-2">{product.stars}.0</span>
-                {Array.from({ length: 5 }, (element, index) => {
-                  return (
-                    <span key={index} className=" text-xl">
-                      {index + 1 > product.stars ? (
-                        <RiStarSLine />
-                      ) : (
-                        <RiStarSFill />
-                      )}
-                    </span>
-                  );
-                })}
-              </div>
-              <span className="text-blue-500">{product.ratings} ratings</span>
+      ) : (
+        <Fragment>
+          <div className="flex flex-col md:flex-row">
+            <div className="rounded-xl md:w-1/2 shadow-xl">
+              <img
+                src={product?.img || ""}
+                alt={product?.name || "Product"}
+                className="rounded-xl p-10 w-full h-full "
+                loading="lazy"
+              />
             </div>
-            <div className="h-0.5 bg-gray-400" />
+
+            <div className="flex flex-col gap-5 py-5 md:py-0 md:px-10 md:w-1/2 justify-evenly">
+              <div>
+                <Link to={"/"}>
+                  <p className=" text-blue-500">More products</p>
+                </Link>
+                <h1 className="text-4xl">{product?.name || "Product"}</h1>
+                <div className="flex flex-wrap py-2 items-center">
+                  <div className="flex text-yellow-500 items-center pr-4">
+                    <span className="pr-2">{product?.stars || 0}.0</span>
+                    {Array.from({ length: 5 }, (element, index) => {
+                      return (
+                        <span key={index} className=" text-xl">
+                          {index + 1 > product?.stars ? (
+                            <RiStarSLine />
+                          ) : (
+                            <RiStarSFill />
+                          )}
+                        </span>
+                      );
+                    })}
+                  </div>
+                  <span className="text-blue-500">
+                    {product?.ratings || 0} ratings
+                  </span>
+                </div>
+                <div className="h-0.5 bg-gray-400" />
+              </div>
+
+              <div>
+                <h2 className="text-xl">About Machine</h2>
+                <p className=" max-h-64 overflow-scroll">
+                  {product?.description || "Product Description"}
+                </p>
+              </div>
+              <div>
+                <h2 className="text-xl">Price Range</h2>
+                <p className="">Min Price: ₹{product?.minPrice || 0}</p>
+                <p className="">Max Price: ₹{product?.maxPrice || 0}</p>
+              </div>
+              <a
+                href={`https://wa.me/7091045216?text=${message}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="w-full bg-blue-500 rounded-lg text-white p-2 h-10 flex items-center justify-center transition hover:bg-blue-700 hover:text-xl">
+                  Enquire Now
+                </button>
+              </a>
+            </div>
           </div>
 
-          <div>
-            <h2 className="text-xl">About Machine</h2>
-            <p className=" max-h-64 overflow-scroll">{product.description}</p>
+          <div className="py-5 pt-10 overflow-scroll">
+            <h1 className="text-2xl pb-2">Technical Specification:</h1>
+            {product && (
+              <table className="table-auto">
+                <thead>
+                  <tr className="border border-x-0">
+                    {Object.entries(product?.specifications).map((items) => (
+                      <th
+                        className="text-md px-6 py-3 text-center"
+                        key={items[0]}
+                        style={{ minWidth: "12rem" }}
+                      >
+                        {items[0]}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.values(product?.specifications)[0].map((_, index) => (
+                    <tr>
+                      {Object.entries(product?.specifications).map((items) => (
+                        <td className="text-md px-6 py-1 text-center">
+                          {items[1][index]}
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
           </div>
-          <div>
-            <h2 className="text-xl">Price Range</h2>
-            <p className="">Min Price: ₹{product.minPrice}</p>
-            <p className="">Max Price: ₹{product.maxPrice}</p>
-          </div>
-          <a
-            href={`https://wa.me/7091045216?text=${message}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className="w-full bg-blue-500 rounded-lg text-white p-2 h-10 flex items-center justify-center transition hover:bg-blue-700 hover:text-xl">
-              Enquire Now
-            </button>
-          </a>
-        </div>
-      </div>
-
-      <div className="py-5 pt-10 overflow-scroll">
-        <h1 className="text-2xl pb-2">Technical Specification:</h1>
-        <table className="table-auto">
-          <thead>
-            <tr className="border border-x-0">
-              {Object.entries(product.specifications).map((items) => (
-                <th
-                  className="text-md px-6 py-3 text-center"
-                  key={items[0]}
-                  style={{ minWidth: "12rem" }}
-                >
-                  {items[0]}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {Object.values(product.specifications)[0].map((_, index) => (
-              <tr>
-                {Object.entries(product.specifications).map((items) => (
-                  <td className="text-md px-6 py-1 text-center">
-                    {items[1][index]}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+        </Fragment>
+      )}
     </div>
   );
 };
