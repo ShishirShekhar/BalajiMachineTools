5c617b9e6a3ed08b8d00fa8d70c5424db7ab45b0
ShishirShekhar <sspdav02@gmail.com>
2023-12-15 00:39:42 +0530

    updated footer link and deatils

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,24 +8,31 @@ import whatsapp from "../images/whatsapp.png";
 
 const Footer = () => {
   return (
-    <div id="footer" className="py-4 bg-blue-500 text-white flex flex-col md:flex-row justify-evenly items-center">
+    <div
+      id="footer"
+      className="py-4 bg-blue-500 text-white flex flex-col md:flex-row justify-evenly items-center"
+    >
       <div className="flex flex-wrap justify-evenly items-center w-full py-5 md:py-0 md:w-1/2">
         <div>
           <img src={logo} alt="logo" className="w-48" />
-          <p className="text-xl font-bold">Sachkhand Machine Tools</p>
+          <p className="text-xl font-bold">Balaji Machine Tools</p>
         </div>
         <div>
           <h1 className="text-2xl">Contact Details</h1>
           <div className="py-2 flex flex-col justify-evenly h-44">
             <p className="text-xl">Address:</p>
             <p>
-              Delux Pulli, G.T Road,
-              <br /> Batala - 143505, Punjab, India
+              G.T Road, Batala <br />
+              143505, Punjab, India
             </p>
             <p className="text-xl">Phone No:</p>
-            <p>08037627280</p>
+            <a href="tel:+918437680240">
+              <p>8437680240</p>
+            </a>
             <p className="text-xl">Email:</p>
-            <p>sachkhandworkshop@gmail.com</p>
+            <a href="mailto:salesbalajimachines@gmail.com">
+              <p>salesbalajimachines@gmail.com</p>
+            </a>
           </div>
         </div>
       </div>
@@ -52,35 +59,47 @@ const Footer = () => {
         <div>
           <h1 className="text-2xl">Connect</h1>
           <div className="py-2 flex flex-col justify-evenly h-44">
-            <a href="https://www.facebook.com">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.facebook.com/ShishirShekhar0/"
+            >
               <div className="flex items-center">
                 <img src={facebook} alt="facebook" className="w-8" />
                 <p>Facebook</p>
               </div>
             </a>
 
-            <a href="https://www.instagram.com">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/sol_shishir"
+            >
               <div className="flex items-center">
                 <img src={instagram} alt="instagram" className="w-8" />
                 <p>Instagram</p>
               </div>
             </a>
 
-            <a href="https://www.linkedin.com">
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/shishir-shekhar">
               <div className="flex items-center">
                 <img src={linkedin} alt="linkedin" className="w-8" />
                 <p>Linkedin</p>
               </div>
             </a>
 
-            <a href="https://www.twitter.com">
+            <a target="_blank" rel="noreferrer" href="https://www.twitter.com/ShishirShekhar0">
               <div className="flex items-center">
                 <img src={twitter} alt="twitter" className="w-8" />
                 <p>Twitter</p>
               </div>
             </a>
 
-            <a href="https://www.whatsapp.com">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://wa.me/+918437680240"
+            >
               <div className="flex items-center">
                 <img src={whatsapp} alt="whatsapp" className="w-8" />
                 <p>Whatsapp</p>
