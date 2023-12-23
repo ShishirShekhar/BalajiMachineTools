c1e4d227bd870111dcb5dfe165751aedaa0fac93
ShishirShekhar <sspdav02@gmail.com>
2023-12-23 13:43:31 +0530

    removed hamburger menu

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { RiCloseFill } from "react-icons/ri";
 import logo from "../images/white_logo.png";
 
 const Nav = () => {
-  const [menu, setMenu] = useState(false);
   const location = useLocation();
-
-  const switchMenu = () => {
-    setMenu((prev) => !prev);
-  };
 
   return (
     <div
@@ -38,32 +30,6 @@ const Nav = () => {
           <a href="/#footer">Contact Us</a>
         </p>
       </div>
-      <button className="text-xl md:hidden" onClick={switchMenu}>
-        {menu ? <RiCloseFill onClick={switchMenu} /> : <RxHamburgerMenu />}
-      </button>
-      {menu && (
-        <div className="absolute bg-black bg-opacity-75 w-screen h-screen top-0 left-0 flex flex-col justify-center items-center text-3xl font-bold">
-          <RiCloseFill
-            className="absolute top-5 right-10"
-            onClick={switchMenu}
-          />
-          <p className="hamburger-option" onClick={switchMenu}>
-            <a href="/#home">Home</a>
-          </p>
-          <p className="hamburger-option" onClick={switchMenu}>
-            <Link to="/products">Our Products</Link>
-          </p>
-          <p className="hamburger-option" onClick={switchMenu}>
-            <a href="/#about">About Us</a>
-          </p>
-          <p className="hamburger-option" onClick={switchMenu}>
-            <a href="/#gallery">Gallery</a>
-          </p>
-          <p className="hamburger-option" onClick={switchMenu}>
-            <a href="/#footer">Contact Us</a>
-          </p>
-        </div>
-      )}
     </div>
   );
 };
