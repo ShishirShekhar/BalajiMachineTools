98456fd4fe6d0a393a4fc1d14f71d89c89a42725
ShishirShekhar <sspdav02@gmail.com>
2024-01-02 15:30:32 +0530

    imporved table

--- client/src/components/ProductDesc/Table.jsx
+++ client/src/components/ProductDesc/Table.jsx
@@ -1,15 +1,15 @@
 const Table = ({ product }) => {
   return (
-    <table className="table-auto">
+    <table className="table-auto w-full border-collapse border border-gray-400">
       <thead>
-        <tr className="border border-x-0">
-          {Object.entries(product?.specifications).map((items) => (
+        <tr className="bg-gray-200">
+          {Object.entries(product?.specifications).map(([key]) => (
             <th
-              className="text-md px-6 py-3 text-center"
-              key={items[0]}
-              style={{ minWidth: "10rem" }}
+              className="text-md px-2 py-3 text-center border border-gray-400"
+              key={key}
+              style={{ minWidth: "5rem" }}
             >
-              {items[0]}
+              {key}
             </th>
           ))}
         </tr>
@@ -17,9 +17,12 @@ const Table = ({ product }) => {
       <tbody>
         {Object.values(product?.specifications)[0].map((_, index) => (
           <tr key={index}>
-            {Object.entries(product?.specifications).map((items) => (
-              <td className="text-md px-6 py-1 text-center" key={items}>
-                {items[1][index]}
+            {Object.entries(product?.specifications).map(([key, value]) => (
+              <td
+                className="text-md px-2 py-2 text-center border border-gray-400"
+                key={key}
+              >
+                {value[index]}
               </td>
             ))}
           </tr>
