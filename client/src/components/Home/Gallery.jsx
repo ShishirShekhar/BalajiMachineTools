c9918fc6772b10c9814b1934f6dce0e2faede75d
ShishirShekhar <sspdav02@gmail.com>
2024-01-03 04:28:04 +0530

    added slider to gallery

--- client/src/components/Home/Gallery.jsx
+++ client/src/components/Home/Gallery.jsx
@@ -1,29 +1,111 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { products } from "../../constants/products";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+const NextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <button
+      className={className}
+      style={{
+        ...style,
+        color: "#fff",
+        height: "100%",
+        cursor: "pointer",
+        zIndex: 1,
+        transition: "background-color 0.3s",
+      }}
+      onClick={onClick}
+    >
+      <FaChevronRight />
+    </button>
+  );
+};
+
+const PrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <button
+      className={className}
+      style={{
+        ...style,
+        color: "#fff",
+        height: "100%",
+        cursor: "pointer",
+        zIndex: 1,
+        transition: "background-color 0.3s",
+      }}
+      onClick={onClick}
+    >
+      <FaChevronLeft />
+    </button>
+  );
+};
 
 const Gallery = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    pauseOnHover: true,
+    cssEase: "linear",
+    autoplaySpeed: 3000,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="py-10 px-5 md:px-10" id="gallery">
-      <h1 className="text-4xl text-center font-semibold py-10">Gallery</h1>
-      <div className="flex flex-wrap justify-evenly gap-5">
-        {products.slice(0, 8).map((product) => {
-          return (
-            <div
-              className="box-shadow rounded-xl p-4 w-40 h-40 md:w-72 md:h-72"
-              key={product.id}
-            >
-              <Link to={`/products/${product.id}`}>
-                <img
-                  src={product.img}
-                  alt={product.name}
-                  className="w-full h-full p-2 hover:p-0 transition-all"
-                />
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+    <div className="py-10 px-5 md:px-10 bg-blue-500" id="gallery">
+      <h1 className="text-4xl text-center font-semibold py-10 text-white">
+        Gallery
+      </h1>
+      <Slider {...settings} className="relative">
+        {products.slice(0, 8).map((product) => (
+          <div key={product.id} className="rounded-xl p-4 h-72 md:h-72 mx-2">
+            <Link to={`/products/${product.id}`}>
+              <img
+                src={product.img}
+                alt={product.name}
+                className="w-full h-full transition-transform hover:scale-105 rounded-xl border"
+              />
+            </Link>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
