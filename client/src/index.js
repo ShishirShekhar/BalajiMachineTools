5d040ccfa86cb4659b8fd9170b4f6b5d22d1c022
ShishirShekhar <sspdav02@gmail.com>
2024-01-03 13:46:10 +0530

    add speed insights

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Get the root element to render the app
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 // Render the app inside a StrictMode component
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
